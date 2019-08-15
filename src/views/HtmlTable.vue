c1d7bac1db8f8f921b17fb1e3d36f4549f034221
tsubota <tsubotak@eli-sys.jp>
2019-08-15 18:26:15 +0900

    add HtmlTable

--- src/views/HtmlTable.vue
+++ src/views/HtmlTable.vue
@@ -1,5 +1,89 @@
 <template>
-  <div class="html-table">
+  <div class="html-table mx-5">
     <h1>Html Table</h1>
+
+    <table class="table table-bordered">
+      <thead>
+        <th></th>
+        <th>コード<br>レポートNo</th>
+        <th>サービス名</th>
+        <th>国</th>
+        <th>利用月</th>
+        <th>登録日</th>
+        <th>総再生<br>回数</th>
+        <th>純収益</th>
+        <th>総会員数<br>MAU</th>
+        <th>金額合計/[報告]</th>
+        <th>金額合計/[計算]</th>
+        <th>前回報告<br>金額合計</th>
+        <th>取込<br>状況</th>
+        <th>操作</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>▼</td>
+          <td>0010-0056</td>
+          <td>株式会社レコチョク</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>5,919,592</td>
+          <td>5,919,592</td>
+          <td>839,760</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>　▼</td>
+          <td>XXX1</td>
+          <td>レコチョク MusicStore</td>
+          <td>JP</td>
+          <td colspan="2">DL / 通常 / ハイレゾ / 映像</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>744,160</td>
+          <td>744,160</td>
+          <td>91,000</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>1</td>
+          <td>シングル</td>
+          <td>JP</td>
+          <td>2019/4</td>
+          <td>2019/5/10 12:35</td>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+          <td>100,000</td>
+          <td>100,000</td>
+          <td>5,000</td>
+          <td>完了</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>2</td>
+          <td>アルバム</td>
+          <td>JP</td>
+          <td>2019/4</td>
+          <td>2019/5/10 12:35</td>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+          <td>120,000</td>
+          <td>120,000</td>
+          <td>6,000</td>
+          <td>完了</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
