d49347b64d392e4e73cc88b2c08a626c4feb8efc
tsubo <ktsubota@gmail.com>
2019-08-17 20:40:02 +0900

    入れ子になっている行の描画を実装

--- src/components/TreeTable.vue
+++ src/components/TreeTable.vue
@@ -3,7 +3,7 @@ export default {
   name: 'TreeTable',
 
   props: {
-    rows: {
+    datas: {
       type: Array,
       required: true,
     },
@@ -11,25 +11,56 @@ export default {
 
   // eslint-disable-next-line
   render(h) {
-    // const rows = this.rows || [];
     return (
       <table class="table table-bordered">
         <thead>
           <tr>
-            <th class="expand-cursor"></th>
-            {this.$slots.header}
+            <th class="header-cursor"></th>
+            { this.$slots.header }
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <i class="lv-1 fas fa-angle-down"></i>
-            </td>
-            {this.$slots.lv1}
-          </tr>
+          { this.renderRows(this.datas, 1, []) }
         </tbody>
       </table>
     );
+  },
+
+  methods: {
+    renderRows(datas, level, accumulator) {
+      return datas.reduce((acc, data) => {
+        const row = this.renderRow(data, level);
+        acc.push(row);
+        if ('children' in data) {
+          // 入れ子になっている row をレンダリング
+          return this.renderRows(data.children, level + 1, acc);
+        }
+        return acc;
+      }, accumulator);
+    },
+    renderRow(data, level) {
+      return (
+        <tr class={ this.levelClass(data, level) }>
+          <td>
+            <i class={ this.iconClass(data, level) }></i>
+          </td>
+          {
+            // 親コンポーネントの slot で icon カラム以外の td をレンダリング
+            (`rowLevel_${level}` in this.$scopedSlots) && (
+              this.$scopedSlots[`rowLevel_${level}`](data)
+            )
+          }
+        </tr>
+      );
+    },
+    levelClass(data, level) {
+      return ('children' in data) ? `level-${level}` : '';
+    },
+    iconClass(data, level) {
+      const icon = 'fas fa-angle-down';
+      const levelClass = this.levelClass(data, level);
+      return ('children' in data) ? `${levelClass} ${icon}` : '';
+    },
   },
 };
 </script>
@@ -39,16 +70,17 @@ export default {
 .table td {
   padding: 5px 8px;
 }
-.expand-cursor {
+.header-cursor {
   width: 10px;
 }
-.lv-1 {
+i.level-1 {
   margin-left: 0px;
 }
-.lv-2 {
-  margin-left: 12px;
+i.level-2 {
+  margin-left: 10px;
 }
-.lv-3 {
-  margin-left: 24px;
+i.level-3 {
+  margin-left: 20px;
+  margin-right: 1px;
 }
 </style>
--- src/views/TreeTable.vue
+++ src/views/TreeTable.vue
@@ -2,7 +2,8 @@
   <div class="tree-table mx-5">
     <h1>Tree Table</h1>
 
-    <tree-table :rows="rows">
+    <tree-table :datas="datas">
+
       <template v-slot:header>
         <th>Col-1</th>
         <th>Col-2</th>
@@ -10,13 +11,33 @@
         <th>Col-4</th>
         <th>Col-5</th>
       </template>
-      <template v-slot:lv1>
-        <td>Lv1-R1-C1</td>
-        <td colspan="4">Lv1-R1-C2</td>
+
+      <template v-slot:rowLevel_1="slotProps">
+        <td>{{ slotProps.col_1 }}</td>
+        <td colspan="4">{{ slotProps.col_2 }}</td>
       </template>
-      <template v-slot:lv2="slotProps"></template>
-      <template v-slot:lv3="slotProps"></template>
-      <template v-slot:lv4="slotProps"></template>
+
+      <template v-slot:rowLevel_2="slotProps">
+        <td>{{ slotProps.col_1 }}</td>
+        <td>{{ slotProps.col_2 }}</td>
+        <td colspan="3">{{ slotProps.col_3 }}</td>
+      </template>
+
+      <template v-slot:rowLevel_3="slotProps">
+        <td>{{ slotProps.col_1 }}</td>
+        <td>{{ slotProps.col_2 }}</td>
+        <td>{{ slotProps.col_3 }}</td>
+        <td colspan="4">{{ slotProps.col_4 }}</td>
+      </template>
+
+      <template v-slot:rowLevel_4="slotProps">
+        <td>{{ slotProps.col_1 }}</td>
+        <td>{{ slotProps.col_2 }}</td>
+        <td>{{ slotProps.col_3 }}</td>
+        <td>{{ slotProps.col_4 }}</td>
+        <td>{{ slotProps.col_5 }}</td>
+      </template>
+
     </tree-table>
   </div>
 </template>
@@ -30,22 +51,22 @@ export default {
   },
   data() {
     return {
-      rows: [
+      datas: [
         {
           col_1: 'Lv1-R1-C1',
           col_2: 'Lv1-R1-C2',
-          _items: [
+          children: [
             {
               col_1: 'Lv2-R2-C1',
               col_2: 'Lv2-R2-C2',
               col_3: 'Lv2-R2-C3',
-              _items: [
+              children: [
                 {
                   col_1: 'Lv3-R3-C1',
                   col_2: 'Lv3-R3-C2',
                   col_3: 'Lv3-R3-C3',
                   col_4: 'Lv3-R3-C4',
-                  _items: [
+                  children: [
                     {
                       col_1: 'Lv4-R4-C1',
                       col_2: 'Lv4-R4-C2',
@@ -69,18 +90,18 @@ export default {
         {
           col_1: 'Lv1-R6-C1',
           col_2: 'Lv1-R6-C2',
-          _items: [
+          children: [
             {
               col_1: 'Lv2-R7-C1',
               col_2: 'Lv2-R7-C2',
               col_3: 'Lv2-R7-C3',
-              _items: [
+              children: [
                 {
                   col_1: 'Lv3-R8-C1',
                   col_2: 'Lv3-R8-C2',
                   col_3: 'Lv3-R8-C3',
                   col_4: 'Lv3-R8-C4',
-                  _items: [
+                  children: [
                     {
                       col_1: 'Lv4-R9-C1',
                       col_2: 'Lv4-R9-C2',
